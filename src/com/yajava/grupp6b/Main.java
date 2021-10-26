49aca7f4b4975dc6f345c3385579bb2050968021
Simon Ulander <devsimonulander@gmail.com>
2021-10-26 13:31:35 +0200

    Testing the print method in main.

--- src/com/yajava/grupp6b/Main.java
+++ src/com/yajava/grupp6b/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		GameBoard gameboard = new GameBoard();
 		gameboard.setPoint(1, 1, 1); // test
-		
+		PrintBoard.printBoard(gameboard);
 	}
 
 }
