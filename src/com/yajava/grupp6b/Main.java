91b3eb440286a7460a41d4a1770be1d757361a25
Simon Ulander <devsimonulander@gmail.com>
2021-10-27 10:14:10 +0200

    Added gameplay.

--- src/com/yajava/grupp6b/Main.java
+++ src/com/yajava/grupp6b/Main.java
@@ -3,9 +3,44 @@ package com.yajava.grupp6b;
 public class Main {
 
 	public static void main(String[] args) {
+		// Initialize the board:
 		GameBoard gameboard = new GameBoard();
-		gameboard.setPoint(1, 1, 1); // test
-		PrintBoard.printBoard(gameboard);
+		
+		boolean running = true;
+		int[] currentmove;
+		
+		while (running) {
+			//Show the board:
+			PrintBoard.printBoard(gameboard);
+			
+			// Check for draw:
+			if (CheckWin.draw(gameboard) == 0) {
+				System.out.println("It's a draw!");
+				running = false;
+				break;
+			}
+			
+			// X makes a move and checks if it resulted in a win:
+			currentmove = GameInput.takeInput("X");
+			gameboard.setPoint(currentmove[0], currentmove[1], 1);
+			if (CheckWin.WinX(gameboard))
+			{
+				System.out.println("X wins!");
+				running = false;
+				break;
+			}
+			
+			// O makes a move and checks if it resulted in a win:
+			PrintBoard.printBoard(gameboard);
+			currentmove = GameInput.takeInput("O");
+			gameboard.setPoint(currentmove[0], currentmove[1], 2);
+			if (CheckWin.WinO(gameboard))
+			{
+				System.out.println("O wins!");
+				running = false;
+				break;
+			}
+		}
 	}
 
 }
