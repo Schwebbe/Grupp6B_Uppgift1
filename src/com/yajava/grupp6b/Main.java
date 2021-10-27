5fda1d7c40200ef18abe791aba985f954edce668
Simon Ulander <devsimonulander@gmail.com>
2021-10-28 00:48:01 +0200

    Added the option to start as X, O, or random.

--- src/com/yajava/grupp6b/Main.java
+++ src/com/yajava/grupp6b/Main.java
@@ -1,6 +1,54 @@
 package com.yajava.grupp6b;
 
+import java.util.Scanner;
+
 public class Main {
+	
+	public static String[] whoStart() {
+		String[] X = {"X","O"};
+		String[] O = {"O","X"};
+		
+		
+		System.out.println("Who should start X, O, or random?");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String input = scan.nextLine();
+		
+		while (!(input.toLowerCase().equals("x") || input.toLowerCase().equals("o") || input.toLowerCase().equals("random"))) {
+			System.out.println("Valid input is X, O or random: ");
+			input = scan.nextLine();
+		}
+		
+		scan.close();
+		
+		if (input.toLowerCase().equals("x")) {
+			return X;
+		}
+		
+		if (input.toLowerCase().equals("o")) {
+			return O;
+		}
+		
+		if (input.toLowerCase().equals("random")) {
+			if (Math.random() >= 0.5) {
+				return O;
+			}
+		}
+		
+		return X;
+	}
+	
+	public static int retVal(String s) {
+		if (s.equals("X")) {
+			return 1;
+		}
+		return 2;
+	}
+	
+	public static void makeMove(GameBoard gb) {
+		
+	}
 
 	public static void main(String[] args) {
 		// Initialize the board:
@@ -9,6 +57,7 @@ public class Main {
 		boolean running = true;
 		int[] currentmove;
 		
+		String[] starter = whoStart();
 		while (running) {
 			//Show the board:
 			PrintBoard.printBoard(gameboard);
@@ -22,8 +71,8 @@ public class Main {
 			}
 			
 			// X makes a move and checks if it resulted in a win:
-			currentmove = GameInput.takeInput("X");
-			gameboard.setPoint(currentmove[0], currentmove[1], 1);
+			currentmove = GameInput.takeInput(starter[0]);
+			gameboard.setPoint(currentmove[0], currentmove[1], retVal(starter[0]));
 			if (CheckWin.WinX(gameboard))
 			{
 				PrintBoard.printBoard(gameboard);
@@ -34,8 +83,8 @@ public class Main {
 			
 			// X makes a move and checks if it resulted in a win:
 			PrintBoard.printBoard(gameboard);
-			currentmove = GameInput.takeInput("O");
-			gameboard.setPoint(currentmove[0], currentmove[1], 2);
+			currentmove = GameInput.takeInput(starter[1]);
+			gameboard.setPoint(currentmove[0], currentmove[1], retVal(starter[1]));
 			if (CheckWin.WinO(gameboard))
 			{
 				PrintBoard.printBoard(gameboard);
