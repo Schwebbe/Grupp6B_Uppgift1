84a3dd2898935eee679203695546ae096b3dd6fb
xiaolumalander <xiaolu.malander@gmail.com>
2021-10-28 13:43:20 +0200

    refresh again.

--- src/com/yajava/grupp6b/GameInput.java
+++ src/com/yajava/grupp6b/GameInput.java
@@ -1,12 +1,24 @@
 package com.yajava.grupp6b;
 
-import java.util.Locale;
 import java.util.Scanner;
 
+/**
+ * Helper class to take and validate input
+ *
+ * @author xiaolu malander
+ * @version 1.0
+ */
 public class GameInput {
     private static final Scanner scanner = new Scanner(System.in);
+
+    //arranged to match the gameBoard layout for easy conversion
     private static final String[][] validInput = {{"A1","A2","A3"},{"B1","B2","B3"},{"C1","C2","C3"}};
 
+    /**
+     * Prompts the player to enter a move and validates the input against the validInput array.
+     * @param player current player
+     * @return int array holding valid move coordinates
+     */
     public static int[] takeInput(String player) {
         boolean isValid = false;
         int[] move = {1,1};
